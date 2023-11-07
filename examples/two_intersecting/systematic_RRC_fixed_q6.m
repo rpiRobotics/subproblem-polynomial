d9085658378868a772347df95b44b20c719d5e3b
Alex Elias <aelias36@yahoo.com>
2023-11-07 16:31:59 -0500

    maple: RRC fixed q6

--- examples/two_intersecting/systematic_RRC_fixed_q6.m
+++ examples/two_intersecting/systematic_RRC_fixed_q6.m
@@ -1,8 +1,41 @@
-kin = define_RRC_fixed_q6()
+kin = define_RRC_fixed_q6();
+
 kin.P(:,end) = 0;
 kin.P(1,5) = round(kin.P(1,5)*1000)/1000;
 %%
 q = zeros([7 1]);
+
+%% Symbolic pose
+ex = [1;0;0];
+ey = [0;1;0];
+ez = [0;0;1];
+zv = [0;0;0];
+
+syms a b g
+alpha = a;
+beta = b;
+gamma = g;
+R_06 = rot(ey, gamma)*rot(ez, beta)*rot(kin.H(:,6), alpha);
+
+assert(dot(ez,kin.H(:,6)) == 0)
+
+syms t1 t2 t3 real
+p_0T = [t1 t2 t3]' + kin.P(:,1) - R_06*kin.P(:,7);
+
+%% Easy pose
+
+R_06 = eye(3);
+p_0T = [30;20;20];
+%% Hard pose
+
+R_06 = rot(ey, deg2rad(30))*rot(ez, deg2rad(20))*rot(kin.H(:,6), deg2rad(10));
+R_06 = double(R_06);
+p_0T = [30.12;20.34;20.56];
+
+%%
+R_06 = rot(ey, deg2rad(0))*rot(ez, deg2rad(20))*rot(kin.H(:,6), deg2rad(10));
+R_06 = double(R_06);
+p_0T = [30.12;20.34;20.56];
 %% Plot robot pose
 diagrams.setup;
 hold on
@@ -25,8 +58,6 @@ kin_num = kin;
 kin_num.P = double(kin.P);
 kin_num.H = double(kin.H);
 
-R_06 = eye(3);
-p_0T = [30;20;20];
 
 [Q, is_LS_vec] = IK.IK_2_intersecting(R_06, p_0T, kin_num)
 xlabel("q_4")
@@ -38,5 +69,7 @@ ylabel("e(q_4)")
 q = Q(:,1)
 vpa(R_06_t - R_06)
 vpa(T_t - p_0T)
+tan(Q(4,:)/2)
+
 %%
 polynomial_IK.two_intersecting(kin,R_06, p_0T, 'RRC_fixed_q6_eqns.txt')
